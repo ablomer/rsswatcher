c08a7cd00c808b60ce6495f40cc2e46e03e7a89f
Augusto Blomer <augusto.blomer@gmail.com>
2025-01-19 21:13:03 -0500

    Removed dotenv use and dependency

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,4 @@
-import { config } from 'dotenv';
-import { fileURLToPath } from 'url';
-import path from 'path';
 import { Server } from './server.js';
-
-// Load environment variables from .env file in the project root
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const rootDir = path.resolve(__dirname, '../..');
-config({ path: path.join(rootDir, '.env') });
 
 async function startApp() {
   const server = new Server();
